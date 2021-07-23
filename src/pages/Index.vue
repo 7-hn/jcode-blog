47dee57aacd0543d288c567e3ab9e08422b37ec2
jcode <746026935@qq.com>
2021-07-23 18:14:33 +0800

    文章列表和文章详情

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,64 +19,27 @@
     <!-- Main Content -->
     <div class="container">
       <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="col-lg-8 col-md-10 mx-auto" v-for="edge in $page.posts.edges" :key="edge.node.id">
           <div class="post-preview">
-            <a href="post.html">
+            <g-link :to="'/post/' + edge.node.id">
               <h2 class="post-title">
-                Man must explore, and this is exploration at its greatest
+                {{ edge.node.title }}
               </h2>
               <h3 class="post-subtitle">
-                Problems look mighty small from 150 miles up
+
               </h3>
-            </a>
-            <p class="post-meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 24, 2019</p>
+            </g-link>
+            <p class="post-meta">Posts by
+              <a href="#" v-for="name in edge.node.created_bies">{{ name.firstname + name.lastname }}</a>
+              {{ edge.node.created_at }}</p>
+            <p>
+              <a href="#" v-for="tag in edge.node.tags" :key="tag.id">{{ tag.title }}</a>
+            </p>
           </div>
-          <hr>
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-              </h2>
-            </a>
-            <p class="post-meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on September 18, 2019</p>
-          </div>
-          <hr>
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Science has not yet mastered prophecy
-              </h2>
-              <h3 class="post-subtitle">
-                We predict too much for the next year and yet far too little for the next ten.
-              </h3>
-            </a>
-            <p class="post-meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on August 24, 2019</p>
-          </div>
-          <hr>
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">
-                Failure is not an option
-              </h2>
-              <h3 class="post-subtitle">
-                Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-              </h3>
-            </a>
-            <p class="post-meta">Posted by
-              <a href="#">Start Bootstrap</a>
-              on July 8, 2019</p>
-          </div>
-          <hr>
           <!-- Pager -->
-          <div class="clearfix">
-            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-          </div>
+
+          <pager :info="$page.posts.pageInfo" />
+          <hr />
         </div>
       </div>
     </div>
@@ -84,14 +47,50 @@
   </layout>
 </template>
 
+<page-query>
+query ($page: Int) {
+  posts: allStrapiPost (perPage: 1, page: $page) @paginate {
+    pageInfo {
+      totalPages,
+      currentPage
+    }
+    edges {
+      node {
+        id
+        title
+        tags {
+            id
+            title
+          }
+        created_bies {
+            id
+            firstname
+            lastname
+          }
+        created_at
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import { Pager } from 'gridsome'
+
 export default {
   name: 'HomePage',
   metaInfo: {
     title: 'Hello, world!'
+  },
+  components: {
+    Pager
   }
 }
 </script>
 
 <style>
+nav a {
+  display: inline-block;
+  width: 50px;
+}
 </style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,58 @@
+<template>
+  <layout>
+    <!-- Page Header -->
+    <header class="masthead" :style="{
+      backgroundImage: `url(http://localhost:1337${$page.post.cover.url})`
+    }">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+              <h1>{{ $page.post.title }}</h1>
+              <span class="meta">{{ $page.post.created_at }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto" v-html="$page.post.content">
+          </div>
+        </div>
+      </div>
+    </article>
+  </layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  post: strapiPost (id: $id) {
+    id
+    title
+    content
+    cover {
+      url
+    }
+    tags {
+      id
+      title
+    }
+    created_at
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: 'PostPage'
+}
+</script>
+
+<style scoped>
+
+</style>
